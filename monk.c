77aa63e8b9bd525578aa0be72680f26869fa1296
Alexandru Calugari <afishr00@gmail.com>
2020-05-15 01:23:54 +0300

    add reading one line from file

--- monk.c
+++ monk.c
@@ -1,5 +1,9 @@
 /*** includes ***/
 
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE
+
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
@@ -216,16 +220,29 @@ int getWindowSize(int *rows, int *cols) {
 
 /*** file i/o ***/
 
-void editorOpen()
+void editorOpen(char *filename)
 {
-	char *line = "Hello, world!";
-	ssize_t linelen = 13;
+	FILE *fp = fopen(filename, "r");
+	if (!fp) die("fopen");
 
-	E.row.size = linelen;
-	E.row.chars = malloc(linelen + 1);
-	memcpy(E.row.chars, line, linelen);
-	E.row.chars[linelen] = '\0';
-	E.numrows = 1;
+	char *line = NULL;
+	size_t linecap = 0;
+	ssize_t linelen;
+
+	linelen = getline(&line, &linecap, fp);
+	if (linelen != -1)
+	{
+		while (linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r')) 
+			linelen--;
+		
+		E.row.size = linelen;
+		E.row.chars = malloc(linelen + 1);
+		memcpy(E.row.chars, line, linelen);
+		E.row.chars[linelen] = '\0';
+		E.numrows = 1;
+	}
+	free(line);
+	fclose(fp);
 }
 
 /*** output ***/
@@ -236,7 +253,7 @@ void editorDrawRows(struct abuf *ab) {
 	{
 		if (y >= E.numrows)
 		{
-			if (y == E.screenrows / 3)
+			if (E.numrows == 0 && y == E.screenrows / 3)
 			{
 				char welcome[80];
 				int welcomelen = snprintf(welcome, sizeof(welcome), "Monkey editor -- version %s", MONKEY_VERSION);
@@ -380,11 +397,15 @@ void initEditor()
 	if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
-int main()
+int main(int argc, char *argv[])
 {
 	enableRawMode();
 	initEditor();
-	editorOpen();
+	
+	if (argc >= 2)
+	{
+		editorOpen(argv[1]);
+	}
 
 	while (1)
 	{
