f0a77730255015b84288898e2f9be28f17d32abe
Alexandru Calugari <afishr00@gmail.com>
2020-05-18 00:39:09 +0300

    add handling tabs as one character

--- monk.c
+++ monk.c
@@ -46,6 +46,7 @@ typedef struct erow
 struct editorConfig
 {
 	int cx, cy;
+	int rx;
 	int rowoff;
 	int coloff;
 	int screenrows;
@@ -101,7 +102,7 @@ void disableRawMode()
 
 void enableRawMode()
 {
-	if (	tcgetattr(STDIN_FILENO, &E.orig_termios) == -1)
+	if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1)
 		die("tcgetattr");
 
 	atexit(disableRawMode);
@@ -225,6 +226,19 @@ int getWindowSize(int *rows, int *cols) {
 
 /*** row operations ***/
 
+int editorRowCxtoRx(erow *row, int cx)
+{
+	int rx = 0;
+	int j;
+	for (j = 0; j < cx; j++)
+	{
+		if (row->chars[j] == '\t')
+			rx += (TAB_STOP - 1) - (rx % TAB_STOP);
+		rx++;
+	}
+	return rx;
+}
+
 void editorUpdateRow(erow *row)
 {
 	int tabs = 0;
@@ -297,6 +311,12 @@ void editorOpen(char *filename)
 
 void editorScroll()
 {
+	E.rx = 0;
+	if (E.cy < E.numrows)
+	{
+		E.rx = editorRowCxtoRx(&E.row[E.cy], E.cx);
+	}
+
 	if (E.cy < E.rowoff)
 	{
 		E.rowoff = E.cy;
@@ -305,13 +325,13 @@ void editorScroll()
 	{
 		E.rowoff = E.cy - E.screenrows + 1;
 	}
-	if (E.cx < E.coloff)
+	if (E.rx < E.coloff)
 	{
-		E.coloff = E.cx;
+		E.coloff = E.rx;
 	}
-	if (E.cx >= E.coloff + E.screencols)
+	if (E.rx >= E.coloff + E.screencols)
 	{
-		E.coloff = E.cx - E.screencols + 1;
+		E.coloff = E.rx - E.screencols + 1;
 	}
 }
 
@@ -371,7 +391,7 @@ void editorRefreshScreen()
 	editorDrawRows(&ab);
 
 	char buf[32];
-	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.cx - E.coloff) + 1);
+	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.rx - E.coloff) + 1);
 	abAppend(&ab, buf, strlen(buf));
 
 	abAppend(&ab, "\x1b[?25h", 6);	
@@ -486,6 +506,7 @@ void initEditor()
 {
 	E.cx = 0;
 	E.cy = 0;
+	E.rx = 0;
 	E.rowoff = 0;
 	E.coloff = 0;
 	E.numrows = 0;
