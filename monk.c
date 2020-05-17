3048bd199b9ee9dabeb9bacdf3c1ea0f6f4441c2
Alexandru Calugari <afishr00@gmail.com>
2020-05-18 00:15:00 +0300

    add proper tab rendering w/ spaces

--- monk.c
+++ monk.c
@@ -18,6 +18,7 @@
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 #define MONKEY_VERSION "0.0.1"
+#define TAB_STOP 8
 
 enum editorKey
 {
@@ -37,7 +38,9 @@ enum editorKey
 typedef struct erow
 {
 	int size;
+	int rsize;
 	char *chars;
+	char *render;
 } erow;
 
 struct editorConfig
@@ -222,6 +225,35 @@ int getWindowSize(int *rows, int *cols) {
 
 /*** row operations ***/
 
+void editorUpdateRow(erow *row)
+{
+	int tabs = 0;
+	int j;
+	for (j = 0; j < row->size; j++)
+		if (row->chars[j] == '\t') tabs++;
+
+
+	free(row->render);
+	row->render = malloc(row->size + tabs * (TAB_STOP - 1) + 1);
+
+	int idx = 0;
+	for (j = 0; j < row->size; j++)
+	{
+		if (row->chars[j] == '\t')
+		{
+			row->render[idx++] = ' ';
+			while (idx % TAB_STOP != 0) row->render[idx++] = ' ';
+		}
+		else
+		{
+			row->render[idx++] = row->chars[j];
+		}
+	}
+
+	row->render[idx] = '\0';
+	row->rsize = idx;	
+}
+
 void editorAppendRow(char *s, size_t len)
 {
 	E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
@@ -231,6 +263,11 @@ void editorAppendRow(char *s, size_t len)
 	E.row[at].chars = malloc(len + 1);
 	memcpy(E.row[at].chars, s, len);
 	E.row[at].chars[len] = '\0';
+	
+	E.row[at].rsize = 0;
+	E.row[at].render = NULL;
+	editorUpdateRow(&E.row[at]);
+
 	E.numrows++;
 }
 
@@ -307,10 +344,10 @@ void editorDrawRows(struct abuf *ab)
 		}
 		else
 		{
-			int len = E.row[filerow].size - E.coloff;
+			int len = E.row[filerow].rsize - E.coloff;
 			if (len < 0) len = 0;
 			if (len > E.screencols) len = E.screencols;
-			abAppend(ab, &E.row[filerow].chars[E.coloff], len);
+			abAppend(ab, &E.row[filerow].render[E.coloff], len);
 		}
 		
 		
